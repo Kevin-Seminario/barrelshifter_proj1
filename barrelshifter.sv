46dbd3dd628fd361167829588e45988761079a10
seminariokevin4 <seminariokevin4@gmail.com>
2026-01-25 19:58:36 -0600

    Fixed overflow flag with size 2 vectors.

--- barrelshifter.sv
+++ barrelshifter.sv
@@ -2,7 +2,7 @@
 `default_nettype none
 
 // a simple module that gives an RTL description of the barrelshifter.
-module barrelshifter #(parameter D_SIZE = 4) (
+module barrelshifter #(parameter D_SIZE) (
 	input logic [D_SIZE-1:0]			x_in,
 	input logic [$clog2(D_SIZE)-1:0]	s_in,
 	input logic [2:0]					op_in,
@@ -146,8 +146,8 @@ module long_nor #(parameter V_SIZE) (
 );
   logic [V_SIZE:0] and_interim;
   logic [V_SIZE:0] nor_interim;
-  assign and_interim[0] = 1'b1;
-  assign nor_interim[0] = 1'b1;
+  not (nor_interim[0], in[0]);
+  assign and_interim[0] = nor_interim[0];
   assign out = and_interim[V_SIZE - 1];
   
   genvar i;
